0ea259615e0ab75cf6a70e3b5f2b6e1b757aa0ac
Jakob Odersky <jakob@odersky.com>
2020-04-13 10:24:25 +0200

    Add some doc comments

--- ustats/src/ustats/Histogram.scala
+++ ustats/src/ustats/Histogram.scala
@@ -11,16 +11,20 @@ class Histogram(
     sum: DoubleAdder
 ) {
 
+  /** Record a single data point. */
   def add(value: Double): Unit = {
-    for (i <- 0 until buckets.length) {
+    var i = 0
+    while (i < buckets.length) {
       if (value <= buckets(i)) {
         adders(i).add(1)
       }
+      i += 1
     }
     count.add(1)
     sum.add(value)
   }
 
+  /** Record the time in seconds it takes to run the given function. */
   def time[A](fct: => A): A = {
     val now = System.nanoTime()
     val result = fct
@@ -29,6 +33,7 @@ class Histogram(
     result
   }
 
+  /** Record the time in seconds it takes to complete the given future. */
   def timeAsync[A](
       fct: => Future[A]
   )(implicit ec: ExecutionContext): Future[A] = {
