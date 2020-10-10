f59700877d7539d7cce638daa37d5f3df1616091
Jakob Odersky <jakob@odersky.com>
2020-10-10 18:44:51 +0200

    Allow setting gauges (this is inherently racy)

--- ustats/src/ustats/Gauge.scala
+++ ustats/src/ustats/Gauge.scala
@@ -4,6 +4,10 @@ import java.util.concurrent.atomic.DoubleAdder
 
 /** Gauges may be increased and decreased. */
 class Gauge(private val adder: DoubleAdder) extends AnyVal {
-  def +=(n: Int) = adder.add(n)
-  def -=(n: Int) = adder.add(-n)
+  def +=(n: Double) = adder.add(n)
+  def -=(n: Double) = adder.add(-n)
+  def set(n: Double) = {
+    adder.reset()
+    adder.add(n)
+  }
 }
